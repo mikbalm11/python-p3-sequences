16cd195b65bf70b7f8de20cff94d7287647ee5d6
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-09-03 23:23:00 -0400

    fibonacci function passes all tests!

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    '''Prints the first 'length' Fibonacci numbers.'''
+    fib_numbers = [0, 1]
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print(fib_numbers)
+    else:
+        # recursively print the first 'length' Fibonacci numbers
+        for numbers in range(2, length):
+            fib_numbers.append(sum(fib_numbers[-2:]))
+        print(fib_numbers)
